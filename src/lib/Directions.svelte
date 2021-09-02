1fbf07baa8f7ef3114fe2bf4a8202a44c9804299
Andrew Sellen <andrewmsellen@gmail.com>
2021-09-02 13:27:17 +1000

    feat: set up file structure

---
+++ src/lib/Directions.svelte
@@ -0,0 +1,78 @@
+<script>
+	const linePath =
+		'M23.7124 766.016L18.7661 757.097H14.129L8.25538 760.172L3 755.559V738.644L9.80108 732.801H45.6613V719.884L49.371 700.816V695.28L43.1882 685.131L30.8226 678.98L26.1855 667.294L30.8226 628.543L40.0968 613.166V604.555L44.1156 595.944L45.043 584.564L57.4086 571.34L63.9005 563.959V556.578L62.0457 550.427V537.51L67.6102 525.516L74.4113 521.825L77.8118 508.909L77.1936 474.464L83.9946 462.162V444.632L92.6505 416.953L93.2688 399.423L99.7608 390.196L97.9059 366.516L99.7608 352.368L104.398 346.833L107.798 334.223L117.073 332.378V254.877L131.911 235.194V205.362L144.586 175.838L165.298 160.153L171.79 159.231L178.901 152.772L184.156 138.933L197.14 124.171L227.745 64.507V24.2188';
+
+	let animationStarted = false;
+	const startAnimation = () => {
+		console.log('start');
+		animationStarted = true;
+	};
+
+	// class="path-animation"
+</script>
+
+<div id="map-directions-container">
+	<button on:click={startAnimation}> START </button>
+
+	<svg
+		width="241"
+		height="790"
+		viewBox="0 0 241 790"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			id="path"
+			class={animationStarted ? 'path-animation' : ''}
+			d={linePath}
+			stroke="#3DFA52"
+			stroke-width="5"
+			stroke-linejoin="bevel"
+		/>
+		<circle cx="27" cy="777" r="10.5" stroke="#3DFA52" stroke-width="5" />
+		<circle cx="228" cy="13" r="10.5" stroke="#3DFA52" stroke-width="5" />
+	</svg>
+	<img
+		id="map-directions"
+		src="https://res.cloudinary.com/future-super/image/upload/v1630460878/map.png"
+		alt="Map of Melbourne to Cosgrove in Victoria"
+	/>
+</div>
+
+<style>
+	#map-directions-container {
+		position: relative;
+		width: 100%;
+	}
+
+	#map-directions {
+		width: 100%;
+	}
+
+	svg {
+		right: 7%;
+		top: 10%;
+		height: 80%;
+		position: absolute;
+	}
+
+	path {
+		fill-opacity: 0;
+		stroke-dasharray: 881.51904296875;
+		stroke-dashoffset: 881.51904296875;
+	}
+
+	.path-animation {
+		animation-name: draw;
+		animation-duration: 5s;
+		animation-iteration-count: 1;
+		animation-timing-function: linear;
+		animation-fill-mode: forwards;
+	}
+
+	@keyframes draw {
+		to {
+			stroke-dashoffset: 0;
+		}
+	}
+</style>
---
+++ src/lib/Map.svelte
@@ -0,0 +1,142 @@
+<script>
+	import mapboxgl from 'mapbox-gl';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		mapboxgl.accessToken =
+			'pk.eyJ1IjoiYW5kcmV3LWZzIiwiYSI6ImNrc2p5cXpjaTJpcTIydHJvODdpMWNhY2sifQ.AMPUDx1H1q5K5bik8ICnvQ';
+
+		const map = new mapboxgl.Map({
+			container: 'map',
+			center: [145.24138875562613, -36.59965724996925], // starting position [lng, lat]
+			zoom: 9, // starting zoom
+			style: 'mapbox://styles/mapbox/dark-v10',
+			antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
+		});
+
+		// create a custom style layer to implement the WebGL content
+		const highlightLayer = {
+			id: 'highlight',
+			type: 'custom',
+
+			// method called when the layer is added to the map
+			// https://docs.mapbox.com/mapbox-gl-js/api/#styleimageinterface#onadd
+			onAdd: function (map, gl) {
+				// create GLSL source for vertex shader
+				const vertexSource = `
+uniform mat4 u_matrix;
+attribute vec2 a_pos;
+void main() {
+gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);
+}`;
+
+				// create GLSL source for fragment shader
+				const fragmentSource = `
+void main() {
+gl_FragColor = vec4(0.0, 1.0, 0.0, 0.5);
+}`;
+
+				// create a vertex shader
+				const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+				gl.shaderSource(vertexShader, vertexSource);
+				gl.compileShader(vertexShader);
+
+				// create a fragment shader
+				const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+				gl.shaderSource(fragmentShader, fragmentSource);
+				gl.compileShader(fragmentShader);
+
+				// link the two shaders into a WebGL program
+				this.program = gl.createProgram();
+				gl.attachShader(this.program, vertexShader);
+				gl.attachShader(this.program, fragmentShader);
+				gl.linkProgram(this.program);
+
+				this.aPos = gl.getAttribLocation(this.program, 'a_pos');
+
+				// define vertices of the triangle to be rendered in the custom style layer
+				const topLeft = mapboxgl.MercatorCoordinate.fromLngLat({
+					lng: 145.6102271371005, // x
+					lat: -36.282720302626736 // y
+				});
+				const topRight = mapboxgl.MercatorCoordinate.fromLngLat({
+					lng: 145.6102271371005,
+					lat: -36.33754396413089
+				});
+				const bottomLeft = mapboxgl.MercatorCoordinate.fromLngLat({
+					lng: 145.65390629608015,
+					lat: -36.282720302626736
+				});
+				const bottomRight = mapboxgl.MercatorCoordinate.fromLngLat({
+					lng: 145.65390629608015,
+					lat: -36.33754396413089
+				});
+
+				// create and initialize a WebGLBuffer to store vertex and color data
+				this.buffer = gl.createBuffer();
+				gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+				gl.bufferData(
+					gl.ARRAY_BUFFER,
+					new Float32Array([
+						topLeft.x,
+						topLeft.y,
+						bottomLeft.x,
+						bottomLeft.y,
+						topRight.x,
+						topRight.y,
+						bottomRight.x,
+						bottomRight.y
+					]),
+					gl.STATIC_DRAW
+				);
+			},
+
+			// method fired on each animation frame
+			// https://docs.mapbox.com/mapbox-gl-js/api/#map.event:render
+			render: function (gl, matrix) {
+				gl.useProgram(this.program);
+				gl.uniformMatrix4fv(gl.getUniformLocation(this.program, 'u_matrix'), false, matrix);
+				gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+				gl.enableVertexAttribArray(this.aPos);
+				gl.vertexAttribPointer(this.aPos, 2, gl.FLOAT, false, 0, 0);
+				// gl.enable(gl.BLEND);
+				// gl.blendFunc(61, 250, 82, 100);
+				gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+			}
+		};
+
+		// add the custom style layer to the map
+		map.on('load', () => {
+			map.addLayer(highlightLayer, 'building');
+		});
+
+		/*
+		const map = new mapboxgl.Map({
+			container: 'map', // container ID
+			style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+			center: [145.5643507, -37.345906], // starting position [lng, lat]
+			zoom: 7, // starting zoom
+			pitch: 50
+		});
+		const marker1 = new mapboxgl.Marker().setLngLat([145.5643507, -36.345906]).addTo(map);
+    */
+	});
+</script>
+
+<body>
+	<h1>Cosgrove</h1>
+	<div id="map" />
+</body>
+
+<style>
+	body {
+		margin: 0;
+		padding: 0;
+	}
+	#map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+	}
+</style>
