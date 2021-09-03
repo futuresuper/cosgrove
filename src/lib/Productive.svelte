54e9d51fe9980c4478cf59d2561cf4294b3493d8
Andrew Sellen <andrewmsellen@gmail.com>
2021-09-03 16:15:55 +1000

    feat: productive ag block

--- src/lib/Productive.svelte
+++ src/lib/Productive.svelte
@@ -1,1 +1,35 @@
-<div style="height: 100vh" />
+<div>
+	<h2>Making use of unproductive farmland</h2>
+</div>
+
+<style>
+	div {
+		background-image: url('https://res.cloudinary.com/future-super/image/upload/f_auto,q_auto/v1630642915/unproductive-farmland.png');
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		border-radius: var(--border-radius);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 20px;
+		height: 90vh;
+	}
+	h2 {
+		font-size: var(--l);
+		color: var(--white);
+		text-transform: uppercase;
+		max-width: 600px;
+		text-align: center;
+		padding: 42px 20px 40px 20px;
+		margin: 0 40px;
+		border: 4px solid var(--white);
+		border-radius: 200px;
+	}
+
+	@media (max-width: 490px) {
+		h2 {
+			border-radius: var(--border-radius);
+		}
+	}
+</style>
