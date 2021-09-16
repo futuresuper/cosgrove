6c1ce3acbaf7cf6f3ab4f7f3808bb8639d93326a
Andrew <andrew@myfuturesuper.com.au>
2021-09-16 12:08:34 +1000

    feat: site password while in dev

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,26 +11,64 @@
 	import ThisIsCosgrove from '$lib/ThisIsCosgrove.svelte';
 	import Timeline from '$lib/Timeline.svelte';
 	import Footer from '$lib/Footer.svelte';
+
+	import { onMount } from 'svelte';
+	let password = '';
+	let passwordCorrectOrNotNeeded = false;
+	let showFailure = false;
+	onMount(async () => {
+		console.log('HOST: ' + window.location.hostname);
+		if (!window.location.hostname.includes('cosgrove.futuresuper')) {
+			passwordCorrectOrNotNeeded = true;
+		}
+	});
 </script>
 
 <body>
-	<Header />
-	<ThisIsCosgrove />
-	<Directions />
-	<Details />
-	<Map />
-	<Productive />
-	<Stats />
-	<Rural />
-	<Renewables />
-	<Shifting />
-	<Timeline />
-	<Footer />
+	{#if passwordCorrectOrNotNeeded}
+		<Header />
+		<ThisIsCosgrove />
+		<Directions />
+		<Details />
+		<Map />
+		<Productive />
+		<Stats />
+		<Rural />
+		<Renewables />
+		<Shifting />
+		<Timeline />
+		<Footer />
+	{:else}
+		<!-- else content here -->
+		<div class="password">
+			<h2>Password</h2>
+			<div>
+				<input bind:value={password} />
+				<button
+					on:click={() =>
+						password === 'F0$$ilFr33' ? (passwordCorrectOrNotNeeded = true) : (showFailure = true)}
+					>GO</button
+				>
+			</div>
+
+			{#if showFailure}
+				<p>Nope!</p>
+			{/if}
+		</div>
+	{/if}
 </body>
 
 <style>
 	body {
 		margin: 0;
 		padding: 0;
+	}
+	.password {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 80px;
 	}
 </style>
